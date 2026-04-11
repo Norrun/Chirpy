0b97beb538a47adceab33c9238ee5449bf87c4b8
Norrun <trulvens@tutanota.com>
2026-04-11 20:41:54 +0200

    adding stats middleware

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
 	log.Println("setting up")
+	var conf apiConfig
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", http.StripPrefix("/app",
+		conf.middlewareMetricsInc(
+			http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("/healthz", readinessHandler)
+
+	mux.HandleFunc("/metrics", conf.handlerHitCount)
+
+	mux.HandleFunc("/reset", conf.handlerHitCountReset)
+
 	server := http.Server{Handler: mux,
 		Addr: ":8080"}
 
@@ -24,4 +34,29 @@ func readinessHandler(res http.ResponseWriter, req *http.Request) {
 	res.WriteHeader(http.StatusOK)
 	io.WriteString(res, "OK")
 
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerHitCount(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, fmt.Sprintf("Hits: %x", cfg.fileserverHits.Load()))
+}
+
+func (cfg *apiConfig) handlerHitCountReset(res http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+	res.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	res.WriteHeader(http.StatusOK)
+	io.WriteString(res, "OK")
 }
